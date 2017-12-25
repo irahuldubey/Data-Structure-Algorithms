4fb326bbcab2e0e0f61216443c8c02f6a0875ee2
Rahul Dubey <rd3098@att.com>
2017-12-25 17:50:53 -0500

    Swift_RemoveDuplicatesFromUnsortedArray

---
+++ Swift_RemoveDuplicatesUnsortedArray.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Remove Duplicates from an Unsorted Array.
+
+var unsortedArray = [1,2,4,3,1,6,6]
+
+func removeDuplicatedFromUnsorted(array: [Int]) -> [Int] {
+  var dupDictionary: [Int : Int] =  [Int: Int]()
+  for arrayValue in 0..<array.count {
+    if let dupValue = dupDictionary[arrayValue] {
+      // Discard it..
+    }
+    else{
+      dupDictionary[arrayValue] = 1
+    }
+  }
+  return Array(dupDictionary.keys)
+}
+
+print(removeDuplicatedFromUnsorted(array: unsortedArray))
+//O(n)
