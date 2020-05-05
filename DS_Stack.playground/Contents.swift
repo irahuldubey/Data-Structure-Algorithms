e4e151993bfd08b9aad37ea623439bc5bdbe592d
RAHUL DUBEY <developerrahuldubey@gmail.com>
2020-05-05 00:33:13 -0700

    Updated Stack Code

--- DS_Stack.playground/Contents.swift
+++ DS_Stack.playground/Contents.swift
@@ -1,34 +1,52 @@
-//: Playground - noun: a place where people can play
 
 import UIKit
 
-public struct Stack<T> {
+struct Stack<T> {
   
   private var bucket = [T]()
-
-  public var isEmptyStack: Bool {
-    return bucket.isEmpty
+  
+  // returns if the bucket is empty
+  public var isEmpty: Bool {
+    return bucket.count == 0
+  }
+  
+  public var elements: [T] {
+    return self.bucket
   }
   
   public var count: Int {
     return bucket.count
   }
   
-  public mutating func push(value: T) {
-    bucket.append(value)
-  }
-
-  public mutating func pop() -> T? {
-    return bucket.popLast()
-  }
-  
-  public var top: T? {
+  // returns last element of the array
+  public func peek() throws -> T? {
+    guard bucket.count > 0 else {
+      return nil
+    }
     return bucket.last
   }
   
+  // Complexity: O(1)
+  public mutating func push(value: T) {
+    self.bucket.append(value)
+  }
+  
+  // Complexity: O(1)
+  @discardableResult
+  public mutating func pop() throws -> T? {
+    self.bucket.popLast()
+  }
 }
 
-var stackObj = Stack<Int>.init()
-stackObj.push(value: 5)
-stackObj.push(value: 1)
-print(stackObj.count)
+var stackObject = Stack<Int>.init()
+stackObject.push(value: 10)
+stackObject.push(value: 20)
+stackObject.push(value: 30)
+stackObject.push(value: 40)
+stackObject.push(value: 50)
+print(stackObject.elements)
+try stackObject.pop()
+try stackObject.pop()
+try stackObject.pop()
+try stackObject.pop()
+print(try stackObject.peek() ?? 0)
