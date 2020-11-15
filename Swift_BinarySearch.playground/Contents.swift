458e0b99c0756e9b6525e46e67d966b38301257b
Rahul Dubey <developerrahuldubey@gmail.com>
2020-11-15 13:30:10 -0800

    New DS

--- Swift_BinarySearch.playground/Contents.swift
+++ Swift_BinarySearch.playground/Contents.swift
@@ -1,11 +1,11 @@
 import UIKit
 
-// Iterative method
+//// Iterative method
 func search(_ nums: [Int], _ target: Int) -> Int {
-    
+
     var lowerBound = 0
     var upperBound = nums.count
-    
+
     while upperBound > lowerBound {
         let midIndex = lowerBound + (upperBound - lowerBound) / 2
         print("midIndex", midIndex)
@@ -22,31 +22,40 @@ func search(_ nums: [Int], _ target: Int) -> Int {
 
 print(search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33))
 
-//func binarySearch<T: Comparable>(_ a: [T], key: T, range: Range<Int>) -> Int? {
-//    if range.lowerBound >= range.upperBound {
-//        // If we get here, then the search key is not present in the array.
-//        return nil
-//
-//    } else {
-//        // Calculate where to split the array.
-//        let midIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
-//
-//        // Is the search key in the left half?
-//        if a[midIndex] > key {
-//            return binarySearch(a, key: key, range: range.lowerBound ..< midIndex)
-//
-//        // Is the search key in the right half?
-//        } else if a[midIndex] < key {
-//            return binarySearch(a, key: key, range: midIndex + 1 ..< range.upperBound)
-//
-//        // If we get here, then we've found the search key!
-//        } else {
-//            return midIndex
-//        }
-//    }
-//}
-//
-//let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
-//print(binarySearch(numbers, key: 43, range: 0 ..< numbers.count))    // gives 13
-//
+func binarySearch<T: Comparable>(_ a: [T], key: T, range: Range<Int>) -> Int? {
+    if range.lowerBound >= range.upperBound {
+        // If we get here, then the search key is not present in the array.
+        return nil
+
+    } else {
+        print("range", range)
+
+        // Calculate where to split the array.
+        let myLowerBound = range.lowerBound
+        let myUpperBound = (range.upperBound - range.lowerBound) / 2
+        
+        print(myLowerBound, myUpperBound)
+        
+        let midIndex = myLowerBound + myUpperBound
+        
+        print("mid Index", midIndex)
+
+        // Is the search key in the left half?
+        if a[midIndex] > key {
+            return binarySearch(a, key: key, range: range.lowerBound ..< midIndex)
+
+        // Is the search key in the right half?
+        } else if a[midIndex] < key {
+            return binarySearch(a, key: key, range: midIndex + 1 ..< range.upperBound)
+
+        // If we get here, then we've found the search key!
+        } else {
+            return midIndex
+        }
+    }
+}
+
+let numbers = [2, 3, 5, 7, 11, 13, 17]
+print(binarySearch(numbers, key: 11, range: 0 ..< numbers.count))    // gives 13
+
 
