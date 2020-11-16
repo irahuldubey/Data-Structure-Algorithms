40a434b2062a17c0aaad1b7c608ba9a02c5ef1e9
Rahul Dubey <developerrahuldubey@gmail.com>
2020-11-16 01:04:50 -0800

    Optimized

--- Swift_insert5.playground/Contents.swift
+++ Swift_insert5.playground/Contents.swift
@@ -5,8 +5,7 @@ public func solution(_ N: Int) -> Int {
     if N == 0 { return 0 }
     
     var indexOfminNumber: Int?
-    var arrayOfIntegers = String(N).compactMap({Int(String($0))})
-
+    var arrayOfIntegers = N.digits
     let element = N > 0 ? arrayOfIntegers.min() : arrayOfIntegers.max()
     
     for (index, value) in arrayOfIntegers.enumerated() {
@@ -18,15 +17,20 @@ public func solution(_ N: Int) -> Int {
     
     if let indexOfminNumber = indexOfminNumber {
         arrayOfIntegers.insert(5, at: indexOfminNumber)
-        if let value = Int(arrayOfIntegers.compactMap { $0.description }
-            .joined(separator: "")) {
-            return N > 0 ? value : -value
-        }
+        let value = arrayOfIntegers.reduce(0) { return $0*10 + $1 }
+        return N > 0 ? value : -value
     }
+    
     return 0
 }
 
-print(solution(505))
+extension BinaryInteger {
+    var digits: [Int] {
+        return String(describing: self).compactMap { Int(String($0)) }
+    }
+}
+
+print(solution(127))
 print(solution(5))
 print(solution(50))
 print(solution(05))
