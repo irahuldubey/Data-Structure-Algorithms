60ed62ccece802aa7abe4c3d456e44797acc81b1
Rahul Dubey <developerrahuldubey@gmail.com>
2021-03-07 18:50:11 -0800

    Swift_Parentheses

--- Swift_Parentheses.playground/Contents.swift
+++ Swift_Parentheses.playground/Contents.swift
@@ -4,27 +4,29 @@ class Solution {
     
     func isValid(_ s: String) -> Bool {
         let s = Array(s)
-    var stack = [ String ] ()
-    
-    for c in s {
-        switch c {
-        case "(":
-            stack.append(")")
-        case "{":
-            stack.append("}")
-        case "[":
-            stack.append("]")
-        default:
-            if stack.isEmpty {
-                return false
-            }
-            let poplastitem = stack.popLast()
-            if let pop = poplastitem, pop != String(c)   {
-                return false
+        var stack = [String]()
+        for c in s {
+            switch c {
+                case "(":
+                    stack.append(")")
+                case "{":
+                    stack.append("}")
+                case "[":
+                    stack.append("]")
+                default:
+                    if stack.isEmpty {
+                        return false
+                    }
+                    let poplastitem = stack.popLast()
+                    print("\(String(describing: poplastitem!)), \(String(c))")
+                    if let pop = poplastitem, pop != String(c)   {
+                        return false
+                    }
             }
         }
-    }
-    return stack.isEmpty
-
+        return stack.isEmpty
     }
 }
+
+
+print(Solution().isValid("(]"))
